52041112edd3fd2e6a1b5e20416c2f5136deed7b
oliverxyy <oliverxyy@gmail.com>
2016-08-24 15:14:38 +0800

    update table column

--- start.sh
+++ start.sh
@@ -12,9 +12,9 @@ stocks=(
 )
 #set interval,default unit is second
 interval=5 #s/m/h
-stock_column_name="股票名\t当前价\t涨幅值\t涨幅比\t开盘价\t收盘价\t最高价\t最低价\t成交股w\t成交额w\t\n"
+stock_column_name="股票名\t当前价\t涨幅值\t涨幅比\t开盘价\t收盘价\t最高价\t最低价\t成交股w\t买卖股差w\t\n"
 #stock_column_name="股票名\t当前价\t涨幅\t开盘价\t收盘价\t最高价\t最低价\t买一价\t卖一价\t成交股w\t成交额w\t买一股\t买一价\t买二股\t买二价\t买三股\t买三价\t卖一股\t卖一价\t卖二股\t卖二价\t卖二股\t卖二价\t卖三股\t卖三价\t当前时间\t\n"
-#stock_column_name="股票名称\t开盘价\t收盘价\t当前价\t最高价\t最低价\t买一价\t卖一价\t成交股\t成交额\t买一股\t买一价\t买二股\t买二价\t买三股\t买三价\t买四股\t买四价\t买五股\t买五价20\t卖一股\t卖一价\t卖二股\t卖二价\t卖二股\t卖二价\t卖三股\t卖三价\t卖四股\t卖四价\t日期\t时间\t\n"
+#stock_column_name="股票名称\t开盘价\t收盘价\t当前价\t最高价\t最低价\t买一价\t卖一价\t成交股\t成交额\t买一股\t买一价\t买二股\t买二价\t买三股\t买三价\t买四股\t买四价\t买五股\t买五价20\t卖一股\t卖一价\t卖二股\t卖二价\t卖三股\t卖三价\t卖四股\t卖四价\t卖五股\t卖五价\t日期\t时间\t\n"
 
 # use sina stock api
 req="http://hq.sinajs.cn/list="
@@ -25,7 +25,7 @@ url=$(echo $req${stocks[*]}|sed 's/ /,/g')
 function printRow(){
     row=$1
     #echo "$row"|awk -F ',' '{if ( $4 < $3 ) c="\033[32m";else c="\033[31m"; printf("%-s\t\033[33m%-s\033[0m\t"c"%.2f\033[0m\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\t%.f\t%.f\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\t%-s\t\n",$1,$4,($4-$3)/$3*100,$2,$3,$5,$6,$7,$8,$9/10000,$10/10000,$11,$12,$13,$14,$15,$16,$21,$22,$23,$24,$25,$26,$27,$28,$32)}'
-    echo "$row"|awk -F ',' '{if ( $4 < $3 ) c="\033[32m";else c="\033[31m"; printf("%-s\t\033[33m%-s\033[0m\t"c"%.2f\033[0m\t"c"%.2f\033[0m\t%-s\t%-s\t%-s\t%-s\t%.f\t%.f\t\n",$1,$4,$4-$3,($4-$3)/$3*100,$2,$3,$5,$6,$9/10000,$10/10000)}'
+    echo "$row"|awk -F ',' '{if ( $4 < $3 ) c="\033[32m";else c="\033[31m"; printf("%-s\t\033[33m%-s\033[0m\t"c"%.2f\033[0m\t"c"%.2f\033[0m\t%-s\t%-s\t%-s\t%-s\t%.f\t\033[31m%.2f\033[0m\t\n",$1,$4,$4-$3,($4-$3)/$3*100,$2,$3,$5,$6,$9/10000,(($11+$13+$15+$17+$19)-($21+$23+$25+$27+$29))/10000)}'
 }
 
 function show(){
